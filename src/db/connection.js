ff9df2cecdf5916229a100d8c647f0633f9b0796
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-01-26 15:33:01 +0000

    Connected app to mongodb using mongoose

---
+++ src/db/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connection = async () => {
+    try {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log("Connection Made")
+    } catch (error){
+        console.log(error);
+    }
+}
+
+connection();
